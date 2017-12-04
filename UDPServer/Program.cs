7d097272f8485b44e87128fa3f0ba893b3e6de25
Frank <Frank@FRANKPC>
2017-12-04 08:35:26 -0500

    Add project files.

---
+++ UDPServer/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Declare variables
+            List<User> userList = new List<User>();
+            UdpClient udpServer = new UdpClient(8080);
+            List<string> userString = new List<string>();
+            
+
+
+            Console.WriteLine("Running...");
+            while (true)
+            {
+                //Check to see if we have any new connections and receive any bytes.
+                IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                try
+                {
+                    //Receive bytes, translate them to a string. And find the username in the string.
+                    byte[] receivedBytes = udpServer.Receive(ref remoteEndPoint);
+                    string returnData = Encoding.ASCII.GetString(receivedBytes);
+                    string userName = returnData.Split(':')[0];
+
+                    //Here we check to see if the user we just received bytes from, has already connected or not.
+                    if (!userString.Contains(userName))
+                    {
+                        //If this is a new user, then we want to create a new one and add them to the user list.
+                        User newUser = new User();
+                        userString.Add(userName);
+                        newUser.UserName = userName;
+                        newUser.Port = remoteEndPoint.Port;
+                        //newUser.Port = 8085;
+                        newUser.UserIP = remoteEndPoint.Address;
+                        newUser.UserEndPoint = new IPEndPoint(remoteEndPoint.Address, 8085);
+                        userList.Add(newUser);
+                        Console.WriteLine(userName.ToString() + ": Connected");
+                    }
+                    else
+                    {
+                        //If they are not a new user, then we want to send the message out to all users currently connected.
+                        byte[] byteMessage = Encoding.ASCII.GetBytes(returnData);
+
+                        //For each user in our list we want to try to send the message.
+                        foreach (User user in userList)
+                        {
+                            try
+                            {
+                                udpServer.Send(byteMessage, byteMessage.Length, user.UserEndPoint);
+                            }
+                            catch (Exception exception)
+                            {
+                                Console.WriteLine(exception);
+                            }
+                        }
+
+                        Console.WriteLine(returnData);
+                    }
+
+
+
+                }
+                catch (Exception exception)
+                {
+                    
+                }
+
+            }
+
+        }
+    }
+}
---
+++ UDPServer/User.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPServer
+{
+    class User
+    {
+        #region Fields
+
+        private string _userName;
+        private IPAddress _userIP;
+        private int _port;
+        private IPEndPoint _userEndPoint;
+
+        #endregion
+
+        #region Properties
+
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+
+        public IPAddress UserIP
+        {
+            get { return _userIP; }
+            set { _userIP = value; }
+        }
+
+        public int Port
+        {
+            get { return _port; }
+            set { _port = value; }
+        }
+
+        public IPEndPoint UserEndPoint
+        {
+            get { return _userEndPoint; }
+            set { _userEndPoint = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public User()
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+    }
+}
